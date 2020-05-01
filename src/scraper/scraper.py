d9888b3f56577677830acb3242be82aed61b8ca7
Tyler Sparr <tyler.sparr@gmail.com>
2020-05-01 18:12:59 -0400

    Black or GTFO

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,28 +1,31 @@
-#! usr/bin/env python3
+#!/usr/bin/env python3
 import praw
 import pandas as pd
 import datetime as dt
 
 
 def main():
-    reddit = praw.Reddit(client_id='./.envrc/id', \
-                        client_secret= './.envrc/key', \
-                        user_agent='DnD Memes', \
-                        username='./.envrc/user', \
-                        password='./.envrc/password')
+    reddit = praw.Reddit(
+        client_id="./.envrc/id",
+        client_secret="./.envrc/key",
+        user_agent="DnD Memes",
+        username="./.envrc/user",
+        password="./.envrc/password",
+    )
 
-    topics_dict = { "title":[], \
-                    "score":[], \
-                    "id":[], "url":[], \
-                    "comms_num": [], \
-                    "created": [], \
-                    "body":[]}
+    topics_dict = {
+        "title": [],
+        "score": [],
+        "id": [],
+        "url": [],
+        "comms_num": [],
+        "created": [],
+        "body": [],
+    }
 
-    subreddit = reddit.subreddit('dndmemes')
+    subreddit = reddit.subreddit("dndmemes")
 
     top_subreddit = subreddit.top(limit=25)
-
-
 
     for submission in top_subreddit:
         topics_dict["title"].append(submission.title)
@@ -34,8 +37,8 @@ def main():
         topics_dict["body"].append(submission.selftext)
 
     topics_data = pd.DataFrame(topics_dict)
-    topics_data.to_csv('top25.csv', index=False)
+    topics_data.to_csv("top25.csv", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
