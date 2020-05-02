4e5ac3e6ae10a93640972358f2ac9571523efa98
Zach Myers <zachmyers3@gmail.com>
2020-05-02 14:41:08 -0400

    remove unneeded id, will use reddit post id

--- src/database/Database.py
+++ src/database/Database.py
@@ -5,11 +5,9 @@ db = SqliteDatabase("parser.db")
 
 
 class ParseData(Model):
-    # auto inc id, uses pre_save hook to autoinc
-    pid = IntegerField()
+    rid = CharField()
     title = CharField()
     score = IntegerField()
-    rid = CharField()
     url = CharField()
     comments = IntegerField()
     created = IntegerField()
@@ -17,11 +15,3 @@ class ParseData(Model):
 
     class Meta:
         database = db
-
-
-@pre_save(sender=ParseData)
-def on_save_handler(model_class, instance, created):
-    # find max value of temp_id in model
-    # increment it by one and assign it to model instance object
-    next_value = ParseData.select(fn.Max(ParseData.pid))[0].temp_id + 1
-    instance.pid = next_value
