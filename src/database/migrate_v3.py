e291dc67cf3a89be1c7dbe08de41c107bd377227
Zach Myers <zachmyers3@gmail.com>
2020-05-11 23:06:09 -0400

    added subreddit field and migration, created population utility for db fields

---
+++ src/database/migrate_v3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from dotenv import find_dotenv, load_dotenv
+from peewee import PostgresqlDatabase, TextField
+from playhouse.migrate import PostgresqlMigrator, migrate
+import os
+
+
+load_dotenv(find_dotenv())
+my_db = PostgresqlDatabase(
+    os.getenv("POSTGRES_DATABASE"),
+    user=os.getenv("POSTGRES_USERNAME"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT"),
+)
+migrator = PostgresqlMigrator(my_db)
+
+subreddit = TextField(null=True)
+
+migrate(migrator.add_column("redditpost", "subreddit", subreddit))
---
+++ src/get_author_subreddit.py
@@ -0,0 +1,34 @@
+from database import RedditPost
+from scraper import RedditScraper
+
+from dotenv import find_dotenv, load_dotenv
+import os
+
+
+def main():
+    posts = RedditPost.select()
+    load_dotenv(find_dotenv())
+
+    scraper = RedditScraper(
+        os.getenv("REDDIT_APP_ID"),
+        os.getenv("REDDIT_SECRET_KEY"),
+        os.getenv("REDDIT_USERAGENT"),
+        os.getenv("REDDIT_USERNAME"),
+        os.getenv("REDDIT_PASSWORD"),
+    )
+
+    for post in posts:
+        if post.author and post.subreddit:
+            print(f'Skipping {post.rid} because data already exists')
+            continue
+        # get reddit post from PRAW
+        print(f'<{post.rid}> {post.title}')
+        r_post = scraper.get_post_by_rid(post.rid)
+        print(f'{r_post.author} {r_post.subreddit}')
+        post.author = r_post.author
+        post.subreddit = r_post.subreddit
+        post.save()
+
+
+if __name__ == "__main__":
+    main()
