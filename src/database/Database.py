261a057be5085135674381cb04edf0585e12d46d
Zach Myers <zachmyers3@gmail.com>
2020-05-14 13:35:51 -0400

    setting up migration to PonyORM

--- src/database/Database.py
+++ src/database/Database.py
@@ -1,11 +1,14 @@
 from dotenv import find_dotenv, load_dotenv
-from peewee import PostgresqlDatabase
 import os
-
+from pony.orm import Database, Required, db_session
+import datetime
 
 load_dotenv(find_dotenv())
-db = PostgresqlDatabase(
-    os.getenv("POSTGRES_DATABASE"),
+
+db = Database()
+
+db.bind(
+    provider='postgres',
     user=os.getenv("POSTGRES_USERNAME"),
     password=os.getenv("POSTGRES_PASSWORD"),
     host=os.getenv("POSTGRES_HOST"),
@@ -13,12 +16,23 @@ db = PostgresqlDatabase(
 )
 
 
-class Database:
-    db = db
+class RedditPost(db.Entity):
+    rid = Required(str)
+    title = Required(str)
+    author = Required(str)
+    score = int
+    url = Required(str)
+    comments = int
+    created = Required(int)
+    body = str
+    post_date = datetime
+    subreddit = datetime
 
-    def connect(self):
-        self.db.connect()
 
-    def create(self, table_list):
-        with self.db:
-            self.db.create_tables(table_list)
+@db_session
+def get_post(rid):
+    post = RedditPost[rid]
+    return post
+
+
+
---
+++ src/database_old/RedditPost.py
@@ -0,0 +1,50 @@
+from peewee import CharField, DateField, IntegerField, Model, TextField, fn
+
+from datetime import date, timedelta
+
+from .Database import db
+
+today = date.today()
+
+
+class RedditPost(Model):
+    rid = CharField(unique=True)
+    title = TextField()
+    author = TextField()
+    score = IntegerField()
+    url = CharField()
+    comments = IntegerField()
+    created = IntegerField()
+    body = CharField()
+    post_date = DateField(null=True)
+    subreddit = TextField(null=True)
+
+    class Meta:
+        database = db
+
+    def check_post_date(self, post):
+        if post.post_date is not None:
+            last_posted = today - post.post_date
+        else:
+            last_posted = timedelta(days=1000, minutes=0, seconds=00)
+
+        return last_posted
+
+    def get_random_post(self):
+        query = self.select().order_by(fn.Random())
+        post = query.get()
+
+        last_posted = self.check_post_date(post)
+
+        while last_posted < timedelta(days=30):
+            query = self.select().order_by(fn.Random())
+            post = query.get()
+            last_posted = self.check_post_date(post)
+
+        return post
+
+    def update_post_date(self, id):
+        self.set_by_id(f"{id}", {"post_date": today})
+
+    def __str__(self):
+        return f"<{self.rid}> {self.title}"
