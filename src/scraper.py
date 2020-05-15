4b29a826149b1f8ff72a318f1d22e7fecbaa5683
Zach Myers <zachmyers3@gmail.com>
2020-05-14 22:08:32 -0400

    clean up commented code

--- src/scraper.py
+++ src/scraper.py
@@ -6,16 +6,10 @@ from dotenv import find_dotenv, load_dotenv
 
 from database.Database import create_post_from_reddit
 
-# from database.Database import Database
-# from database.RedditPost import RedditPost
 from scraper.RedditScraper import RedditScraper
 
 
 def main():
-    # create database
-    # db = Database()
-    # db.create([RedditPost])
-
     # load .env environment files
     load_dotenv(find_dotenv())
     # create scraper
@@ -33,23 +27,6 @@ def main():
         if not post.selftext:
             db_post = create_post_from_reddit(post)
             print(db_post)
-            # create database object
-            # tmp_reddit_post = RedditPost(
-            #     rid=post.id,
-            #     author=post.author,
-            #     title=post.title,
-            #     score=post.score,
-            #     url=post.url,
-            #     comments=post.num_comments,
-            #     created=post.created,
-            #     body=post.selftext,
-            #     subreddit=post.subreddit,
-            # )
-
-    # random_meme = RedditPost().get_random_post()
-    # random_meme_url = random_meme.url
-    # print(random_meme_url)
-    # RedditPost().update_post_date(random_meme.id)
 
 
 if __name__ == "__main__":
