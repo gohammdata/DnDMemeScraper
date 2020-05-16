1762a4bbed3828c511f4ced7470b4c3c75cc0aa6
TSPARR <TSPARR@users.noreply.github.com>
2020-05-16 00:58:07 -0400

    Start with 404's (#49)
    * Start with 404's
    
    * Requested fixes

---
+++ src/check_existence.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import requests
+
+from database.Database import RedditPost, get_all_posts
+from pony.orm import db_session
+
+
+@db_session
+def main():
+    posts = get_all_posts()
+
+    for post in posts:
+        response = requests.get(post.url)
+        print(f"Verifying {post.rid}: {post.title}")
+        if response.status_code == 404:
+            RedditPost.delete(post.rid)
+            print(f"Deleted {post.rid}: {post.title}")
+
+
+if __name__ == "__main__":
+    main()
